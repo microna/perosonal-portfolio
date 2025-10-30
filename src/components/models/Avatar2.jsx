56a1c87902afe97b55b1b200f2114829092e124f
microna <kievonfire@gmail.com>
2025-10-30 11:47:05 +0200

    updated avatar

--- src/components/models/Avatar2.jsx
+++ src/components/models/Avatar2.jsx
@@ -4,17 +4,15 @@ Command: npx gltfjsx@6.5.3 boy.glb -T
 Files: boy.glb [1.08MB] > /Users/hsuwinlatt/Desktop/Work/Fullstack Empire/Youtube/threed-dev-portfolio/public/models/boy-transformed.glb [495.53KB] (54%)
 */
 
-import { useGraph } from "@react-three/fiber";
 import { useAnimations, useFBX, useGLTF } from "@react-three/drei";
+import { useEffect, useRef, useMemo } from "react";
 import { SkeletonUtils } from "three-stdlib";
-import { useEffect, useMemo, useRef } from "react";
 
 export function ContactAvatar(props) {
   const group = useRef();
 
-  const { scene } = useGLTF("/models/avatar2-transformed.glb");
+  const { scene } = useGLTF("/models/avatarnew.glb");
   const clone = useMemo(() => SkeletonUtils.clone(scene), [scene]);
-  const { nodes, materials } = useGraph(clone);
 
   const { animations } = useFBX("/models/Breakdance2.fbx");
   animations[0].name = "Dance";
@@ -22,99 +20,13 @@ export function ContactAvatar(props) {
 
   useEffect(() => {
     action.actions["Dance"].play();
-  }, []);
+  }, [action]);
 
   return (
     <group {...props} ref={group} dispose={null}>
-      <primitive object={nodes.Hips} />
-      <skinnedMesh
-        geometry={nodes.Wolf3D_Hair.geometry}
-        material={materials.Wolf3D_Hair}
-        skeleton={nodes.Wolf3D_Hair.skeleton}
-      />
-      <skinnedMesh
-        geometry={nodes.Wolf3D_Glasses.geometry}
-        material={materials.Wolf3D_Glasses}
-        skeleton={nodes.Wolf3D_Glasses.skeleton}
-      />
-      <skinnedMesh
-        geometry={nodes.Wolf3D_Outfit_Bottom.geometry}
-        material={materials.Wolf3D_Outfit_Bottom}
-        skeleton={nodes.Wolf3D_Outfit_Bottom.skeleton}
-      />
-      <skinnedMesh
-        geometry={nodes.Wolf3D_Outfit_Footwear.geometry}
-        material={materials.Wolf3D_Outfit_Footwear}
-        skeleton={nodes.Wolf3D_Outfit_Footwear.skeleton}
-      />
-      <skinnedMesh
-        geometry={nodes.Wolf3D_Outfit_Top.geometry}
-        material={materials.Wolf3D_Outfit_Top}
-        skeleton={nodes.Wolf3D_Outfit_Top.skeleton}
-      />
-      <skinnedMesh
-        name="EyeLeft"
-        geometry={nodes.EyeLeft.geometry}
-        material={materials.Wolf3D_Eye}
-        skeleton={nodes.EyeLeft.skeleton}
-        morphTargetDictionary={nodes.EyeLeft.morphTargetDictionary}
-        morphTargetInfluences={nodes.EyeLeft.morphTargetInfluences}
-      />
-      <skinnedMesh
-        name="EyeRight"
-        geometry={nodes.EyeRight.geometry}
-        material={materials.Wolf3D_Eye}
-        skeleton={nodes.EyeRight.skeleton}
-        morphTargetDictionary={nodes.EyeRight.morphTargetDictionary}
-        morphTargetInfluences={nodes.EyeRight.morphTargetInfluences}
-      />
-      <skinnedMesh
-        name="Wolf3D_Head"
-        geometry={nodes.Wolf3D_Head.geometry}
-        material={materials.Wolf3D_Skin}
-        skeleton={nodes.Wolf3D_Head.skeleton}
-        morphTargetDictionary={nodes.Wolf3D_Head.morphTargetDictionary}
-        morphTargetInfluences={nodes.Wolf3D_Head.morphTargetInfluences}
-      />
-      <skinnedMesh
-        name="Wolf3D_Teeth"
-        geometry={nodes.Wolf3D_Teeth.geometry}
-        material={materials.Wolf3D_Teeth}
-        skeleton={nodes.Wolf3D_Teeth.skeleton}
-        morphTargetDictionary={nodes.Wolf3D_Teeth.morphTargetDictionary}
-        morphTargetInfluences={nodes.Wolf3D_Teeth.morphTargetInfluences}
-      />
-      {/* Add body parts if they exist */}
-      {nodes.Wolf3D_Body && (
-        <skinnedMesh
-          geometry={nodes.Wolf3D_Body.geometry}
-          material={materials.Wolf3D_Skin}
-          skeleton={nodes.Wolf3D_Body.skeleton}
-        />
-      )}
-      {nodes.Wolf3D_Hands && (
-        <skinnedMesh
-          geometry={nodes.Wolf3D_Hands.geometry}
-          material={materials.Wolf3D_Skin}
-          skeleton={nodes.Wolf3D_Hands.skeleton}
-        />
-      )}
-      {nodes.Wolf3D_Arms && (
-        <skinnedMesh
-          geometry={nodes.Wolf3D_Arms.geometry}
-          material={materials.Wolf3D_Skin}
-          skeleton={nodes.Wolf3D_Arms.skeleton}
-        />
-      )}
-      {nodes.Wolf3D_Legs && (
-        <skinnedMesh
-          geometry={nodes.Wolf3D_Legs.geometry}
-          material={materials.Wolf3D_Skin}
-          skeleton={nodes.Wolf3D_Legs.skeleton}
-        />
-      )}
+      <primitive object={clone} />
     </group>
   );
 }
 
-useGLTF.preload("/models/avatar2-transformed.glb");
+useGLTF.preload("/models/avatarnew.glb");
